3bca5004c4b95c77da07af4584c8c00a1d7556b0
Soumya-singh602 <singh.soumya3008@gmail.com>
2026-08-18 10:35:00 +0530

    Secure database credentials with environment variables

--- database/primary.py
+++ database/primary.py
@@ -1,14 +1,28 @@
+import os
+
 import psycopg2
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 PRIMARY_DB_CONFIG = {
-    "host": "127.0.0.1",
-    "port": 5432,
-    "database": "loadbalancer_db",
-    "user": "postgres",
-    "password": "replicater123",
+
+    "host": os.getenv("PRIMARY_DB_HOST"),
+
+    "port": os.getenv("PRIMARY_DB_PORT"),
+
+    "database": os.getenv("PRIMARY_DB_NAME"),
+
+    "user": os.getenv("PRIMARY_DB_USER"),
+
+    "password": os.getenv("PRIMARY_DB_PASSWORD"),
+
 }
 
 
 def get_primary_connection():
+
     return psycopg2.connect(**PRIMARY_DB_CONFIG)
--- database/replica.py
+++ database/replica.py
@@ -1,14 +1,28 @@
+import os
+
 import psycopg2
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 REPLICA_DB_CONFIG = {
-    "host": "127.0.0.1",
-    "port": 5434,
-    "database": "loadbalancer_db",
-    "user": "postgres",
-    "password": "replicater123",
+
+    "host": os.getenv("REPLICA_DB_HOST"),
+
+    "port": os.getenv("REPLICA_DB_PORT"),
+
+    "database": os.getenv("REPLICA_DB_NAME"),
+
+    "user": os.getenv("REPLICA_DB_USER"),
+
+    "password": os.getenv("REPLICA_DB_PASSWORD"),
+
 }
 
 
 def get_replica_connection():
+
     return psycopg2.connect(**REPLICA_DB_CONFIG)
